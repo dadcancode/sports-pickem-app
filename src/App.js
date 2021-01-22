edfe0f53e09e7570f08949f7042826ce2dd24552
Zach Goodrich <zachery.goodrich@gmail.com>
2021-01-21 17:57:28 -0700

    able to communicate with back end and get team data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import axios from 'axios'
 import Banner from "./components/Banner/Banner";
 import PickCard from "./components/PickCard/PickCard";
 import VersusJumbo from "./components/VersusJumbo/VersusJumbo";
 
 
 function App() {
+
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/teams/index')
+      .then(resp => resp.json())
+      .then(json => {
+        console.log(json)
+      })
+  })
+
+
   return (
     <div className="App">
       <Banner />
