581203e0a2efc2fc0af65584ab0e34cc63aa6491
Zach Goodrich <zachery.goodrich@gmail.com>
2021-01-21 21:40:54 -0700

    LoadScreen component

---
+++ src/components/LoadScreen/LoadScreen.js
@@ -0,0 +1,9 @@
+import ReactLoading from 'react-loading';
+
+const LoadScreen = () => {
+    return (
+        <ReactLoading type='spokes' color='blue' height={'30%'} width={'30%'} />
+    )
+}
+
+export default LoadScreen;
