7c4aaaff47b4ab881e86404a05f70c5464fc29c8
Zach Goodrich <zachery.goodrich@gmail.com>
2021-01-25 14:15:30 -0700

    styled choose season list

--- src/components/ChooseSeason/ChooseSeason.js
+++ src/components/ChooseSeason/ChooseSeason.js
@@ -2,17 +2,19 @@ import { useQuery } from '@apollo/react-hooks';
 import { useEffect, useState } from 'react';
 import SeasonTab from './SeasonTab';
 
+import './ChooseSeason.css';
+
 
 const ChooseSeason = (props) => {
 
     return (
         <div className='container-fluid'>
             <div className='row'>
-                <div className='col-12'>
+                <div className='col-12 p-3 text-center'>
                     <h3>Choose Historic NFL Season</h3>
                 </div>
-                <div className='col-12'>
-                    <div className='list-group'>
+                <div className='col-12 season-col'>
+                    <div className='list-group season-list border border-dark'>
                         {
                             props.seasons ?
                                 props.seasons.map((val) => {
