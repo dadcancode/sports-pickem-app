c456e4829e5064042d5f730e8df3c0aa5919c952
Zach Goodrich <zachery.goodrich@gmail.com>
2021-01-25 15:58:53 -0700

    changed color of loading wheel

--- src/components/LoadScreen/LoadScreen.js
+++ src/components/LoadScreen/LoadScreen.js
@@ -5,7 +5,7 @@ const LoadScreen = () => {
         <div className='container-fluid h-100'>
             <div className='row h-100'>
                 <div className='col-12 h-100 d-flex justify-content-center align-items-center'>
-                    <ReactLoading type='spinningBubbles' color='blue' height={'30%'} width={'30%'} />
+                    <ReactLoading type='spinningBubbles' color='whitesmoke' height={'30%'} width={'30%'} />
                 </div>
             </div>
         </div>
