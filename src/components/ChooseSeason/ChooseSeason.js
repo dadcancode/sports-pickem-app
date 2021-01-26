631f50e34aad3134be419489e11fcc76fe5ee9a3
Zach Goodrich <zachery.goodrich@gmail.com>
2021-01-26 11:22:07 -0700

    styled choose season

--- src/components/ChooseSeason/ChooseSeason.js
+++ src/components/ChooseSeason/ChooseSeason.js
@@ -10,11 +10,11 @@ const ChooseSeason = (props) => {
 
     return (
         <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-12 d-flex justify-content-center align-items-center p-3 imp-text imp-big-resp text-light header-col'>
+            <div className='row justify-content-center align-items-center'>
+                <div className='col-12 d-flex justify-content-center align-items-center p-3 mb-md-4 imp-text imp-big-resp text-light header-col'>
                     Choose Historic NFL Season
                 </div>
-                <div className='col-12 season-col'>
+                <div className='col-md-6 season-col'>
                     <div className='list-group w-100 season-list border border-dark rounded'>
                         {
                             props.seasons ?
